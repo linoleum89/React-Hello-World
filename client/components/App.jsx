3828ea81dc0298b72918e9fff111b341a823db56
Jorge Cortés <jorge.cortes@neoris.com>
2018-09-10 21:49:48 -0500

    add class properties

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default class App extends React.Component {
+	handlerClick = () => {
+		console.log('class properties work')
+	}
 	render() {
 		return (
 			<div style={{ textAlign: 'center' }}>
 				<h1>Hello World</h1>
+				<button onClick={this.handlerClick}>click</button>
 			</div>
 		)
 	}
